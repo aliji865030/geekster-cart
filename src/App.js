366266196737396ab3956991287fb8b9c33a03cc
aliji865030 <abbasalichand786@gmail.com>
2024-03-12 16:52:00 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LandingScreen from "./screen/LandingScreen";
+
+function App() {
+  return (
+    <div>
+      <LandingScreen></LandingScreen>
+    </div>
+  );
+}
+
+export default App;
